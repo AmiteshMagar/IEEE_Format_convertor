41c1ee866f5e8426f3454c9d09ceff99ed416d6a
Amitesh Magar <amitesh.magar@gmail.com>
2020-10-24 23:13:35 +0530

    first commit. created the parameter reader

---
+++ 19122002_Formats.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<sstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	
+	stringstream ss(argv[1]);
+	int T =0;
+	ss >> T;
+	cout << T << endl;
+}
