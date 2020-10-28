f6f6a9b2b06c0e89a7393d0e53ecc9fe5b1e6a17
Amitesh Magar <amitesh.magar@gmail.com>
2020-10-28 21:32:26 +0530

    Tested and added FxP part into main code. works finegit add -A

--- 19122002_Formats.cpp
+++ 19122002_Formats.cpp
@@ -27,6 +27,46 @@ int IntegerFunc(string passed_string){
 	return  (sign * final_number);															//return final number 
 }																							//Working Integer conversion function	
 
+float FloatingFunc(string passed_string, int T, int E){
+	
+	char signBit = passed_string[0];														//storing sign bit as char
+	string mantBitStr = passed_string.substr(1,E);											//storing integerbitsin string format
+	string fracBitStr = passed_string.substr(E+1);											//storing fraction bits in strng format
+
+	int sign = 1;																			//determining and storing sign bit
+	if ( signBit == '1')
+		sign = -1;
+
+	int loopVar1 = mantBitStr.size() - 1;													//initializing first loop variable
+	int Mantissa = 0;																		//variable for final integer part of number
+	int degree = 1;
+	while(loopVar1 >= 0){																	//first loop for cycling through the integer part bit string
+		int temp = 0;																		//temporary integer storage
+		if (mantBitStr[loopVar1] == '1')
+			temp = 1;
+		Mantissa = Mantissa + (degree * temp);												//final integer part key manipulation
+		degree = degree * 2;
+		loopVar1--;																			//decrementing first loop variable
+	}
+
+	int loopVar2 = 0;																		//initializing second loop variable
+	float fraction = 0.0d;																	//float variable for storing final fractional part
+	float frac_degree = 0.5;
+	while(loopVar2 <= (fracBitStr.size()-1)){												//second loop to cycle through the fractional part bits in *descending* order
+		int temp = 0;																		//re-initialized temporary integer storage
+		if(fracBitStr[loopVar2] == '1')
+			temp = 1;
+		fraction = fraction + (frac_degree * temp);											//final fractional part key manipulation
+		frac_degree = frac_degree/2;
+		loopVar2++; 																		//incrementing second loop variable as descending scheme applied
+	}
+	float fin = Mantissa + fraction;														//addition of obtained results of integer and fractional part
+
+	return (sign * fin);																	//return final number
+}																							//Working FxP conversion function
+
+
+
 int main(int argc, char* argv[]){															//driver code
 	
 	stringstream Total(argv[1]);															//String stream for Total (T) parameter passed
